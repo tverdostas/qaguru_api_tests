757e43e4a735d0f365542203bdc969bd787d0084
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-09-16 21:04:21 +0400

    some fixes

--- src/test/java/ReqresApiTests.java
+++ src/test/java/ReqresApiTests.java
@@ -6,55 +6,70 @@ import static org.hamcrest.core.Is.is;
 
 public class ReqresApiTests extends BaseTest {
     @Test
-    void checkTotal() {
+    void checkTotalTest() {
         given()
+                .log().uri()
                 .when()
                 .get("/users")
                 .then()
+                .log().status()
+                .log().body()
                 .body("total", is(12));
     }
 
     @Test
-    void checkPages() {
+    void checkPagesTest() {
         given()
                 .header("x-api-key", apiKey)
+                .log().uri()
                 .when()
                 .get("/users")
                 .then()
+                .log().status()
+                .log().body()
                 .body("total_pages", is(2));
     }
 
     @Test
-    void getUserToken() {
+    void getUserTokenTest() {
         given()
                 .header("x-api-key", apiKey)
                 .when()
+                .log().uri()
                 .post("/login")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(200)
                 .body("token", notNullValue());
     }
 
     @Test
-    void getNonExistentUser() {
+    void getNonExistentUserTest() {
         given()
                 .header("x-api-key", apiKey)
                 .when()
+                .log().uri()
                 .get("/users/200")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(404);
     }
 
     @Test
-    void getClientError() {
+    void getClientErrorTest() {
         String jsonBodyString = "{\"email\": \"tracey.ramo@reqres.in\", \"password\": \"123456\"}";
 
         given()
                 .header("x-api-key", apiKey)
                 .body(jsonBodyString)
                 .when()
+                .log().uri()
                 .post("/login")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(400);
     }
 }
